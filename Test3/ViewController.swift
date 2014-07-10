08664fd2b6ef024211b69f434328976cd34630ae
Philipp Süess <papsueess@n0003165.local>
2014-07-10 10:17:33 +0200

    Initial Commit

---
+++ Test3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test3
+//
+//  Created by Philipp Süess on 10.07.14.
+//  Copyright (c) 2014 Philipp Süess. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
